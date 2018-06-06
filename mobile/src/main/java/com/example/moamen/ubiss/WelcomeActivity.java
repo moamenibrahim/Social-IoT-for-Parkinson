56434706b74147487def8e25d2ed72797bab0eb2
Arash Sattari <arash.sattari@gmail.com>
2018-06-06 11:54:43 +0300

    v1

--- mobile/src/main/java/com/example/moamen/ubiss/WelcomeActivity.java
+++ mobile/src/main/java/com/example/moamen/ubiss/WelcomeActivity.java
@@ -1,7 +1,12 @@
 package com.example.moamen.ubiss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.moamen.ubiss.R.id.startButton;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+    }
+
+
+    public void startTask(View view) {
+        Intent intent = new Intent(this, TaskActivity.class);
+        startActivity(intent);
     }
 }
