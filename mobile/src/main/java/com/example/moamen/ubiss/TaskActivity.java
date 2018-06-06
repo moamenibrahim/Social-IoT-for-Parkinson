389cf3c7f6a459a2338e1b013972f52c0f2a0ae2
moamen <mmibrahem76@gmail.com>
2018-06-06 18:02:51 +0300

    fixes to builds

--- mobile/src/main/java/com/example/moamen/ubiss/TaskActivity.java
+++ mobile/src/main/java/com/example/moamen/ubiss/TaskActivity.java
@@ -6,12 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 
 
 public class TaskActivity extends AppCompatActivity {
-   @Override
-   protected void onCreate(@Nullable Bundle savedInstanceState) {
-                  super.onCreate(savedInstanceState);
-                   setContentView(R.layout.activity_task);
-              }
-    }
+
     private CanvasView canvasView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
