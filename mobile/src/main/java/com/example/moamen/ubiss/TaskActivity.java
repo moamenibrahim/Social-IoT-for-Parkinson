6bac0d92da86a50f8bb7145be77ed586db80ed05
Onur Ozuduru <onur.ozuduru@gmail.com>
2018-06-06 17:36:16 +0300

    Results from drawing.

--- mobile/src/main/java/com/example/moamen/ubiss/TaskActivity.java
+++ mobile/src/main/java/com/example/moamen/ubiss/TaskActivity.java
@@ -1,18 +1,35 @@
 package com.example.moamen.ubiss;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class TaskActivity extends AppCompatActivity {
     private CanvasView canvasView;
+    View view;
+    final int[] loc = new int[2];
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
         canvasView = (CanvasView) findViewById(R.id.canvas);
+        view = (View) findViewById(R.id.view);
+        CanvasView.userError = 0f;
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        view.getLocationOnScreen(loc);
+        Log.println(Log.INFO, "ourX", Integer.toString(loc[0]));
+
+        CanvasView.ourX = loc[0];
+        CanvasView.ourY = view.getBottom();
+
+    }
 
 }
