39e7551da5d0587d89964f4c7c907ce27d7744ee
Onur Ozuduru <onur.ozuduru@gmail.com>
2018-06-06 14:21:41 +0300

    Drawing baby steps.

--- mobile/src/main/java/com/example/moamen/ubiss/TaskActivity.java
+++ mobile/src/main/java/com/example/moamen/ubiss/TaskActivity.java
@@ -6,9 +6,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class TaskActivity extends AppCompatActivity {
+    private CanvasView canvasView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+        canvasView = (CanvasView) findViewById(R.id.canvas);
     }
+
+
 }
